8291dd56611615c8089e1f285b9de8641c07bc54
ThatGuyHughesy <ConorHughes93@gmail.com>
2014-06-06 14:06:00 +0100

    Re-upload

--- Deck.java
+++ Deck.java
@@ -13,48 +13,61 @@ class Deck
 		Result r = new Result();
 		
 		u.getHand();
-		u.newHand();
+		u.newCard();
 		d.getDeal(u);
 		r.getResult(u,d);
 	}
 }		
-		
+
 class User
 {
-	String [] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
-	String [] value = {"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Jack","Queen","King"};
-	int [] deck = {1,2,3,4,5,6,7,8,9,10,10,10,10};
-	int [][] dealt = new int [4][13];
+	//display content on swing
+	private String displayTitle;
+	private String displayContent;
+	String [] suit = {"Clubs", "Diamonds", "Hearts", "Spades"}; //suit names
+	String [] value = {"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Jack","Queen","King"}; //card values
+	int [] deck = {1,2,3,4,5,6,7,8,9,10,10,10,10}; //card int values
+	int [][] dealt = new int [4][13]; //stops cards being dealt twice
 	int answer = 0, dealer = 0, deal = 0, total = 0, newCard;
-	Object[] options = {"Hit","Stand"};
+	private Object[] options = {"Hit","Stand"}; //options for JOptionPane
+	private int firstCardValue;
+	private int firstCardSuit ;
+	private String firstCardName;
+	private int secondCardValue;
+	private int secondCardSuit;
+	private String secondCardName;
 		
 		void getHand()
 		{
-			int firstCardValue = new Random().nextInt(12);
-			int firstCardSuit = new Random().nextInt(3);
-			String firstCardName = value[firstCardValue] + " of " + suit[firstCardSuit];
-			dealt [firstCardSuit] [firstCardValue] = 1;
+			//each card is randomly given a suit and value and a string is created to display it
+			firstCardValue = new Random().nextInt(12);
+			firstCardSuit = new Random().nextInt(3);
+			firstCardName = value[firstCardValue] + " of " + suit[firstCardSuit];
+			dealt [firstCardSuit] [firstCardValue] = 1; //card has been dealt
 			
-			int secondCardValue = new Random().nextInt(12);
-			int secondCardSuit = new Random().nextInt(3);
-			String secondCardName = value[secondCardValue] + " of " + suit[secondCardSuit];
+			//although small probability must make sure the first and second cards aren't the same
+			do
+			{
+				secondCardValue = new Random().nextInt(12);
+				secondCardSuit = new Random().nextInt(3);
+				secondCardName = value[secondCardValue] + " of " + suit[secondCardSuit];
+			}
+			while(dealt [secondCardSuit] [secondCardValue] == 1);
 			dealt [secondCardSuit] [secondCardValue] = 1;
 			
-			total = deck[firstCardValue] + deck[secondCardValue];
+			total = deck[firstCardValue] + deck[secondCardValue]; //total of card's values
 			
 			if(total == 21)
 			{
-				JOptionPane.showMessageDialog(null,
-					"First Card: " + firstCardName + "\nSecond Hand: " + secondCardName + "\nTotal Hand Value: " + total + "\nBlackjack!\nYOU WIN!",
-					"BLACKJACK",
-					JOptionPane.PLAIN_MESSAGE);
-					System.exit(0);
+				displayTitle = "BLACKJACK";
+				displayContent = "First Card: " + firstCardName + "\nSecond Card: " + secondCardName + "\nTotal Hand Value: " + total + "\nBlackjack!\nYOU WIN!";
+				display();
 			}
 			
 			else
 			{
 				answer = JOptionPane.showOptionDialog(null,
-						"First Card: " + firstCardName + "\nSecond Hand: " + secondCardName + "\nTotal Hand Value: " + total + "\n\nWhat Would You Like To Do?",
+						"First Card: " + firstCardName + "\nSecond Card: " + secondCardName + "\nTotal Hand Value: " + total + "\n\nWhat Would You Like To Do?",
 							"Your Hand",
 						JOptionPane.YES_NO_OPTION,
 						JOptionPane.PLAIN_MESSAGE,
@@ -64,9 +77,9 @@ class User
 			}
 		}
 		
-		void newHand()
+		void newCard()//deals new cards if user wants to hit
 		{
-			while(answer!=1)
+			while(answer!=1)//loop until user stands
 			{
 				if(answer == 0)
 				{
@@ -80,31 +93,27 @@ class User
 						newCardSuit = new Random().nextInt(3);
 						newCardName = value[newCardValue] + " of " + suit[newCardSuit];
 					}
-					while(dealt [newCardSuit] [newCardValue] == 1);
+					while(dealt [newCardSuit] [newCardValue] == 1);//ensures cards aren't dealt twice
 					dealt [newCardSuit] [newCardValue] = 1;
 					total += deck[newCardValue];
 					
 					if(total > 21)
 					{
-						JOptionPane.showMessageDialog(null,
-						"New Card: " + newCardName + "\nTotal Hand Value: " + total + "\nBUST!",
-							"BUST",
-						JOptionPane.PLAIN_MESSAGE);
-						System.exit(0);
+						displayTitle = "BUST";
+						displayContent = "New Card: " + newCardName + "\nTotal Hand Value: " + total + "\nBUST!";
+						display();
 					}
 					
 					if(total == 21)
 					{
-						JOptionPane.showMessageDialog(null,
-						"New Card: " + newCardName + "\nTotal Hand Value:  " + total + "\nBlackjack!\nYOU WIN!",
-							"BLACKJACK",
-						JOptionPane.PLAIN_MESSAGE);
-						System.exit(0);
+						displayTitle = "BLACKJACK";
+						displayContent = "New Card: " + newCardName + "\nTotal Hand Value:  " + total + "\nBlackjack!\nYOU WIN!";
+						display();
 					}
 					
 					else
 					{
-					answer = JOptionPane.showOptionDialog(null,
+						answer = JOptionPane.showOptionDialog(null,
 						"New Card: " + newCardName + "\nTotal Hand Value: " + total + "\n\nWould You Like Another Card?",
 							"Your Hand",
 						JOptionPane.YES_NO_OPTION,
@@ -116,16 +125,26 @@ class User
 				}
 			}
 		}
+		
+		void display()//display if bust or blackjack
+		{
+			JOptionPane.showMessageDialog(null,
+			displayContent,
+			displayTitle,
+			JOptionPane.PLAIN_MESSAGE);
+			System.exit(0);
+		}
 }	
 
 class Dealer
 {
 	int dealer = 0;
+	String dealerDisplay;
 
 	void getDeal(User u)
 	{
 		while (dealer < 16)
-		//dealer only stands on 17 or more
+		//dealer only stands on 16 or more
 		{
 			int dealerCardValue = 0;
 			int dealerCardSuit = 0;
@@ -137,10 +156,11 @@ class Dealer
 				dealerCardSuit = new Random().nextInt(3);
 				dealerCardName = u.value[dealerCardValue] + " of " + u.suit[dealerCardSuit];
 			}
-			while(u.dealt [dealerCardSuit] [dealerCardValue] == 1);
+			while(u.dealt [dealerCardSuit] [dealerCardValue] == 1);//ensure cards aren't dealt twice
 			u.dealt [dealerCardSuit] [dealerCardValue] = 1;
 			dealer += u.deck[dealerCardValue];
 			
+			//display what cards the dealer are dealt
 			JOptionPane.showMessageDialog(null,
 			"Dealer's Card: " + dealerCardName + "\nTotal Hand Value: " + dealer,
 			"Dealer's Hand",
@@ -149,53 +169,59 @@ class Dealer
 		
 		if(dealer == 21)
 		{
-			JOptionPane.showMessageDialog(null,
-			"\nBlackjack!\nDEALER WINS!",
-			"BLACKJACK",
-			JOptionPane.PLAIN_MESSAGE);
-			System.exit(0);
+			dealerDisplay = "\nBlackjack!\nDEALER WINS!";
+			display();
 		}
 			
 		if(dealer > 21)
 		{
+			dealerDisplay = "\nDEALER BUSTS!\nYOU WIN!";
+			display();
+		}
+	}
+
+	void display()//display if bust or blackjack
+	{
 			JOptionPane.showMessageDialog(null,
-			"\nBlackjack!\nDEALER BUSTS!\nYOU WIN!",
+			dealerDisplay,
 			"Winner",
 			JOptionPane.PLAIN_MESSAGE);
 			System.exit(0);
-		}
 	}
 }
-		
+
+//displays result of game if both dealer and user stand
 class Result
 {
+	String winnerDisplay;
+	
 	void getResult(User u, Dealer d)
 	{
 		if(u.total == d.dealer)
 		{
-			JOptionPane.showMessageDialog(null,
-			"\nYour Total Hand Value: " + u.total + "\nThe Dealer's Total Hand Value: " + d.dealer + "\n\nYOU DRAW!",
-			"Winner",
-			JOptionPane.PLAIN_MESSAGE);
-			System.exit(0);
+			winnerDisplay = "\n\nYOU DRAW!";
+			display(u, d);
 		}
 				
 		if(u.total > d.dealer)
 		{
-			JOptionPane.showMessageDialog(null,
-			"\nYour Total Hand Value: " + u.total + "\nThe Dealer's Total Hand Value: " + d.dealer + "\n\nYOU WIN!",
-			"Winner",
-			JOptionPane.PLAIN_MESSAGE);
-			System.exit(0);
+			winnerDisplay = "\n\nYOU WIN!";
+			display(u, d);
 		}
 				
 		else 
 		{
-			JOptionPane.showMessageDialog(null,
-			"Your Hand Total Value: " + u.total + "\nThe Dealer's Total Hand Value: " + d.dealer + "\n\nDEALER WINS!",
-			"Winner",
-			JOptionPane.PLAIN_MESSAGE);
-			System.exit(0);
+			winnerDisplay = "\n\nDEALER WINS!";
+			display(u, d);
 		}
+	}
+	
+	void display(User u, Dealer d)//display who wins
+	{
+		JOptionPane.showMessageDialog(null,
+		"Your Hand Total Value: " + u.total + "\nThe Dealer's Total Hand Value: " + d.dealer + winnerDisplay,
+		"Winner",
+		JOptionPane.PLAIN_MESSAGE);
+		System.exit(0);
 	}
 }
